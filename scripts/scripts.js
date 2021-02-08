ce22824edd0702899ebb46344708a9dda079133d
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2021-02-08 18:26:06 -0500

    clean code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,27 @@
-
-
 var opacity = 0;
-var intervalID=0;
-function fadein(){
-    intervalID=setInterval(show,90);
+var intervalID = 0;
+
+function fadein() {
+    intervalID = setInterval(show, 90);
 }
-function show(){   
+
+function show() {
     var section = document.getElementById("catalogo");
     opacity = Number(window.getComputedStyle(section).getPropertyValue("opacity"));
-    console.log("Fade");    
-    if(opacity<1){
-        opacity=opacity+0.1
-        section.style.opacity=opacity;
-    }else{
+    console.log("Fade");
+    if (opacity < 1) {
+        opacity = opacity + 0.1
+        section.style.opacity = opacity;
+    } else {
         clearInterval(intervalID);
     }
 }
 fadein();
 
-function reload(){   
+function reload() {
     location.reload();
 }
-function room(){
+
+function room() {
     window.location = "room.html";
 }
